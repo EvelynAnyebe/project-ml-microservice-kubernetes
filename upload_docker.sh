bc19dbe70cb05fc499ff7d0ca1599f0c3db59531
EC2 Default User <ec2-user@ip-172-31-28-228.ec2.internal>
2022-08-25 19:41:13 +0000

    Work on docker file, run shell scripts and made predictions

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=evelyn2019/udacity_api
 
 # Step 2:  
 # Authenticate & tag
+docker login 
+docker tag=evelyn2019/udacity_api $dockerpath:latest
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push evelyn2019/udacity_api:latest
